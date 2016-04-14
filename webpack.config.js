4342f83b4f731c176e94581d3f7ce76ab6bd1154
delphifeel <delphifeel@gmail.com>
2016-04-14 22:10:26 +0300

    new config

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
             "node_modules"
         ]
     },
-    devtool: 'eval',
+    devtool: "#source-map",
     devServer: {
         contentBase: path.resolve(__dirname, "public"),
         publicPath: "/static/",
